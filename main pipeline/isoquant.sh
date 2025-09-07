e0f7d6dbc1fc9e1baf7d6743143f4da89e2370f3
yerry77 <ybeirui@163.com>
2025-09-07 17:09:47 +0800

    Create isoquant.sh

---
+++ main pipeline/isoquant.sh
@@ -0,0 +1,24 @@
+conda activate isoquant
+isoquant.py \
+  --output ./results \
+  --complete_genedb \
+  --genedb ./reference/gencode.vM25.annotation.gtf \
+  --reference ./reference/GRCm38.p6.genome.fa \
+  --bam ./bam/OVA_TG_hifi.bam ./bam/PBS_TG_hifi.bam \
+  --labels OVA_TG PBS_TG \
+  --prefix TG.hifi \
+  --data_type pacbio \
+  --matching_strategy precise \
+  --splice_correction_strategy default_pacbio \
+  --model_construction_strategy default_pacbio \
+  --transcript_quantification all \
+  --gene_quantification all \
+  --report_novel_unspliced true \
+  --threads 200 \
+  --check_canonical \
+  --sqanti_output \
+  --count_exons
+
+# --bam  路径：放置你的 PacBio HiFi BAM 文件
+# --genedb 参考注释：Gencode vM25
+# --reference 参考基因组：GRCm38.p6
